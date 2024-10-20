8df8d266da469fdc495e9b711e79d715d36559d9
kristinakatriel <kristinakc.nunag@gmail.com>
2024-10-21 00:41:52 +1100

    Added buttons to deck display for: study session, quiz mode, adding a flashcard to deck, editing deck, and deleting the deck

--- flashcards/frontend/src/components/DeckDisplay.jsx
+++ flashcards/frontend/src/components/DeckDisplay.jsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import QuizIcon from '@mui/icons-material/Quiz';
 import { invoke } from '@forge/bridge'; 
 import Button, { IconButton } from '@atlaskit/button/new';
 import CrossIcon from '@atlaskit/icon/glyph/cross';
 import { Flex, Grid, xcss } from '@atlaskit/primitives';
 import Modal, { ModalBody, ModalFooter, ModalHeader, ModalTitle, ModalTransition } from '@atlaskit/modal-dialog';
+import './DeckDisplay.css';
 
 const gridStyles = xcss({
     width: '100%',
@@ -58,79 +61,93 @@ const DeckDisplay = ({ deck }) => {
     };
 
     return (
-        <div className="deck-display-container">
+      <div className='deck-display-container'>
+        <div className='deck-title-and-buttons'>
+          <div className='title-left-buttons'>
             <h1>{updatedDeck.title}</h1>
-            <h2>Flashcards</h2>
-            <h4 className='deck-flashcard-amount'>Flashcards: {updatedDeck.cards?.length || 0}</h4>
-
-            {updatedDeck.cards.length > 0 ? (
-                <div className="card-wrapper">
-                    <ul className="card-list">
-                        {updatedDeck.cards.map((flashcard) => (
-                            <li key={flashcard.id} className="card-item">
-                                <div className="card-link">
-                                    {flashcard.tags && flashcard.tags.length > 0 && (
-                                        <p className="badge blue">{flashcard.tags.join(', ')}</p>
-                                    )}
-                                    <h4 className="card-question">{flashcard.question_text || 'No question available'}</h4>
-                                    <h4 className="card-answer">{flashcard.answer_text || 'No answer available'}</h4>
-                                    <h4 className="card-owner">By {flashcard.owner || 'Unknown'}</h4>
-
-                                    {flashcard.question_image && (
-                                        <img src={flashcard.question_image} alt="Question" className="question-image" />
-                                    )}
-                                    {flashcard.answer_image && (
-                                        <img src={flashcard.answer_image} alt="Answer" className="answer-image" />
-                                    )}
-
-                                    <div className="card-button">
-                                        <EditIcon
-                                            className="card-edit-button"
-                                            onClick={() => handleEdit(flashcard)}
-                                        />
-                                        <DeleteIcon
-                                            className="card-delete-button"
-                                            onClick={() => openModal(flashcard)} // Open modal with the flashcard
-                                        />
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ) : (
-                <p>No flashcards in this deck.</p>
-            )}
-
-            <ModalTransition>
-                {isModalOpen && (
-                    <Modal onClose={closeModal}>
-                        <ModalHeader>
-                            <Grid gap="space.200" templateAreas={['title close']} xcss={gridStyles}>
-                                <Flex xcss={closeContainerStyles} justifyContent="end">
-                                    <IconButton
-                                        appearance="subtle"
-                                        icon={CrossIcon}
-                                        label="Close Modal"
-                                        onClick={closeModal}
-                                    />
-                                </Flex>
-                                <Flex xcss={titleContainerStyles} justifyContent="start">
-                                    <ModalTitle appearance="danger">Are you sure you want to delete this flashcard?</ModalTitle>
-                                </Flex>
-                            </Grid>
-                        </ModalHeader>
-                        <ModalBody>
-                            <p>This action cannot be undone.</p>
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button appearance="subtle" onClick={closeModal}>No</Button>
-                            <Button appearance="danger" onClick={handleDelete}>Yes</Button>
-                        </ModalFooter>
-                    </Modal>
-                )}
-            </ModalTransition>
+            <div className='left-buttons'>
+              <button className='deck-display-add-study-session-icon'><AddIcon fontSize='small'/> Add to study session</button>
+              <button className='deck-display-quiz-icon'><QuizIcon fontSize='small'/>Quiz Mode</button>
+            </div>
+          </div>
+          <div className='right-buttons'>
+          <button className='deck-display-add-flashcard-icon'><AddIcon fontSize='small'/> Add Flashcard</button>
+          <button className='deck-display-edit-icon'><EditIcon fontSize='small'/> Edit Deck</button>
+          <button className='deck-display-delete-icon'><DeleteIcon fontSize='small'/>Delete Deck</button>
+          </div>
         </div>
+
+        <h2>Flashcards</h2>
+        <h4 className='deck-flashcard-amount'>Flashcards: {updatedDeck.cards?.length || 0}</h4>
+
+        {updatedDeck.cards.length > 0 ? (
+            <div className="card-wrapper">
+                <ul className="card-list">
+                    {updatedDeck.cards.map((flashcard) => (
+                        <li key={flashcard.id} className="card-item">
+                            <div className="card-link">
+                                {flashcard.tags && flashcard.tags.length > 0 && (
+                                    <p className="badge blue">{flashcard.tags.join(', ')}</p>
+                                )}
+                                <h4 className="card-question">{flashcard.question_text || 'No question available'}</h4>
+                                <h4 className="card-answer">{flashcard.answer_text || 'No answer available'}</h4>
+                                <h4 className="card-owner">By {flashcard.owner || 'Unknown'}</h4>
+
+                                {flashcard.question_image && (
+                                    <img src={flashcard.question_image} alt="Question" className="question-image" />
+                                )}
+                                {flashcard.answer_image && (
+                                    <img src={flashcard.answer_image} alt="Answer" className="answer-image" />
+                                )}
+
+                                <div className="card-button">
+                                    <EditIcon
+                                        className="card-edit-button"
+                                        onClick={() => handleEdit(flashcard)}
+                                    />
+                                    <DeleteIcon
+                                        className="card-delete-button"
+                                        onClick={() => openModal(flashcard)} // Open modal with the flashcard
+                                    />
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ) : (
+            <p>No flashcards in this deck.</p>
+        )}
+
+        <ModalTransition>
+            {isModalOpen && (
+                <Modal onClose={closeModal}>
+                    <ModalHeader>
+                        <Grid gap="space.200" templateAreas={['title close']} xcss={gridStyles}>
+                            <Flex xcss={closeContainerStyles} justifyContent="end">
+                                <IconButton
+                                    appearance="subtle"
+                                    icon={CrossIcon}
+                                    label="Close Modal"
+                                    onClick={closeModal}
+                                />
+                            </Flex>
+                            <Flex xcss={titleContainerStyles} justifyContent="start">
+                                <ModalTitle appearance="danger">Are you sure you want to delete this flashcard?</ModalTitle>
+                            </Flex>
+                        </Grid>
+                    </ModalHeader>
+                    <ModalBody>
+                        <p>This action cannot be undone.</p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button appearance="subtle" onClick={closeModal}>No</Button>
+                        <Button appearance="danger" onClick={handleDelete}>Yes</Button>
+                    </ModalFooter>
+                </Modal>
+            )}
+        </ModalTransition>
+      </div>
     );
 };
 
