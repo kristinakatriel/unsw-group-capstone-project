008933cbdf1acdca87578dba849634eb391a42ec
kristinakatriel <kristinakc.nunag@gmail.com>
2024-10-25 00:58:28 +1100

    Quiz mode display completed. Added timer, added current flashcards tracker. Fixed cross and check buttons to highlight card with respective colours, and move to next card. Added a Quiz Completed div that shows time take to complete when user goes through all flashcards

--- flashcards/frontend/src/components/QuizMode.jsx
+++ flashcards/frontend/src/components/QuizMode.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
-import ArrowRightIcon from '@atlaskit/icon/glyph/arrow-right';
+import React, { useState, useEffect } from 'react';
 import CrossIcon from '@atlaskit/icon/glyph/cross';
-import QuestionIcon from '@atlaskit/icon/glyph/question';
 import CheckIcon from '@atlaskit/icon/glyph/check';
-import LightbulbIcon from '@atlaskit/icon/glyph/lightbulb'
+import LightbulbIcon from '@atlaskit/icon/glyph/lightbulb';
 import Modal, { ModalBody, ModalFooter, ModalHeader, ModalTitle, ModalTransition } from '@atlaskit/modal-dialog';
 import Button from '@atlaskit/button/new';
 import './QuizMode.css';
@@ -12,79 +9,120 @@ import './QuizMode.css';
 const QuizMode = ({ deck }) => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
-  const [isHintModalOpen, setIsHintModalOpen] = useState(false); 
-
-  const openHintModal = () => setIsHintModalOpen(true);
-  const closeHintModal = () => setIsHintModalOpen(false);
+  const [isHintModalOpen, setIsHintModalOpen] = useState(false);
+  const [cardStatus, setCardStatus] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0); 
+  const [isQuizCompleted, setIsQuizCompleted] = useState(false);
 
   const flashcards = deck.cards;
   const totalCards = flashcards.length;
 
-  const goToPreviousCard = () => {
-    setCurrentCardIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-    setIsFlipped(false); 
-  };
+  useEffect(() => {
+    if (!isQuizCompleted) {
+      const timer = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [isQuizCompleted]);
+
+  const openHintModal = () => setIsHintModalOpen(true);
+  const closeHintModal = () => setIsHintModalOpen(false);
 
   const goToNextCard = () => {
-    setCurrentCardIndex((prevIndex) => Math.min(prevIndex + 1, totalCards - 1));
-    setIsFlipped(false); 
+    if (currentCardIndex < totalCards - 1) {
+      setCurrentCardIndex((prevIndex) => prevIndex + 1);
+    } else {
+      setIsQuizCompleted(true); 
+    }
+    setIsFlipped(false);
+    setCardStatus(null);
+  };
+
+  const handleCorrect = () => {
+    setCardStatus('correct');
+    setTimeout(() => {
+      goToNextCard();
+      setCardStatus(null); 
+    }, 1000);
+  };
+
+  const handleWrong = () => {
+    setCardStatus('wrong');
+    setTimeout(() => {
+      goToNextCard();
+      setCardStatus(null); 
+    }, 1000);
   };
 
   const toggleFlip = () => {
-    setIsFlipped((prevFlipped) => !prevFlipped); 
+    setIsFlipped((prevFlipped) => !prevFlipped);
   };
 
   const handleHintClick = (event) => {
     event.stopPropagation();
-    console.log("HINT BUTTON HAS BEEN PRESSED");
-    console.log(currentCard.hint);
     openHintModal();
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
   };
 
   const currentCard = flashcards[currentCardIndex];
 
   return (
     <div className='quiz-mode-container'>
-      <div className='quiz-mode-title'>
-        <h1>Quiz Mode for {deck.title}</h1>
-      </div>
-      <div className='quiz-mode-progress-bar'></div>
-      <div className='quiz-mode-timer'></div>
-      {currentCard ? (
-        <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={toggleFlip} id="flipCard">
-          <div className='flip-card-inner'>
-            <div className='flip-card-front'>
-              <div className='flip-card-front-hint' onClick={handleHintClick}>
-                <LightbulbIcon />
-                <div class='flip-card-front-hint-hidden'>Click to open Hint!</div>
+      {!isQuizCompleted ? (
+        <>
+          <div className='quiz-mode-title'>
+            <h1>Quiz Mode for {deck.title}</h1>
+          </div>
+          <div className='quiz-mode-information'>
+            <h4 className='quiz-mode-timer'>
+              Time: {formatTime(elapsedTime)}
+            </h4>
+            <h4 className='quiz-mode-flashcard-counter'>
+              Current Flashcard: {currentCardIndex + 1}/{totalCards}
+            </h4>
+          </div>
+          <div
+            className={`flip-card ${isFlipped ? 'flipped' : ''} ${cardStatus === 'correct' ? 'correct-card' : cardStatus === 'wrong' ? 'wrong-card' : ''}`}
+            onClick={toggleFlip}
+            id="flipCard"
+          >
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div className="flip-card-front-hint" onClick={handleHintClick}>
+                  <LightbulbIcon />
+                  <div className="flip-card-front-hint-hidden">Click to open Hint!</div>
+                </div>
+                <h1>{currentCard.question_text}</h1>
               </div>
-              <h1>{currentCard.question_text}</h1>
-            </div>
-            <div className='flip-card-back'>
-              <h1>{currentCard.answer_text}</h1>
+              <div className="flip-card-back">
+                <h1>{currentCard.answer_text}</h1>
+              </div>
             </div>
           </div>
-        </div>
+
+          <div className='quiz-mode-bottom-buttons'>
+            <div className='quiz-mode-wrong-button' onClick={handleWrong}>
+              <CrossIcon />
+            </div>
+            <div className='quiz-mode-correct-button' onClick={handleCorrect}>
+              <CheckIcon />
+            </div>
+          </div>
+        </>
       ) : (
-        <p>No flashcards available.</p>
+        <div className='quiz-completed'>
+          <h1>Quiz completed!</h1>
+          <p>Completed in {formatTime(elapsedTime)}</p>
+        </div>
       )}
-      <div className='quiz-mode-bottom-buttons'>
-        <div className='quiz-mode-left-buttons'>
-          <div className='quiz-mode-prev-button' onClick={goToPreviousCard}>
-            <ArrowLeftIcon />
-          </div>
-        </div>
-        <div className='quiz-mode-middle-buttons'>
-          <div className='quiz-mode-wrong-button'><CrossIcon /></div>
-          <div className='quiz-mode-unsure-button'><QuestionIcon /></div>
-          <div className='quiz-mode-correct-button'><CheckIcon /></div>
-        </div>
-        <div className='quiz-mode-right-buttons'>
-          <div className='quiz-mode-next-button' onClick={goToNextCard}>
-            <ArrowRightIcon />
-          </div>
-        </div>
-      </div>
+
       <ModalTransition>
         {isHintModalOpen && (
           <Modal onClose={closeHintModal}>
@@ -92,7 +130,7 @@ const QuizMode = ({ deck }) => {
               <ModalTitle>Hint</ModalTitle>
             </ModalHeader>
             <ModalBody>
-              {currentCard.hint}
+              {currentCard && currentCard.hint}
             </ModalBody>
             <ModalFooter>
               <Button appearance="primary" onClick={closeHintModal}>
@@ -101,7 +139,7 @@ const QuizMode = ({ deck }) => {
             </ModalFooter>
           </Modal>
         )}
-        </ModalTransition>
+      </ModalTransition>
     </div>
   );
 };
