60dc0ea87643e84dd699bc174bc9465d934c5c97
Zac <z5316780@ad.unsw.edu.au>
2024-11-11 11:45:02 +1100

    ready to merge

--- flashcards/frontend/src/tagGlobalModuleCreate.js
+++ flashcards/frontend/src/tagGlobalModuleCreate.js
@@ -12,7 +12,7 @@ import UnlockIcon from '@atlaskit/icon/glyph/unlock';
 import LockIcon from '@atlaskit/icon/glyph/lock';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import './tagGlobalModuleCreate.css';
+import './glob.css';
 import SearchIcon from '@mui/icons-material/Search';
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
