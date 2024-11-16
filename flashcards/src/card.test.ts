a6625f5775552978b44d7496c87a0985734d32d5
Mohan Das <z5361139@ad.unsw.edu.au>
2024-11-16 19:08:14 +1100

    more tests done

--- flashcards/src/card.test.ts
+++ flashcards/src/card.test.ts
@@ -2,36 +2,177 @@ jest.mock('@forge/api', () => ({
   storage: {
     get: jest.fn(),
     set: jest.fn(),
+    query: jest.fn(() => ({
+      where: jest.fn(() => ({
+        getMany: jest.fn(),
+      })),
+    })),
   },
+  startsWith: jest.fn((prefix) => prefix),
 }));
 
 jest.mock('./helpers', () => ({
   generateId: jest.fn(() => '12345'),
-  getUserName: jest.fn(() => 'Test User'),
+  getUserName: jest.fn(() => 'Freddie'),
   initUserData: jest.fn()
 }));
 
-import {createFlashcard} from './cardResolvers';
+import {createFlashcard, updateFlashcard} from './cardResolvers';
 import { storage } from '@forge/api';
 import { Card, ResolverRequest } from './types';
 import { Resolver } from 'dns';
+import { startsWith } from '@forge/api';
 
 describe('Flashcards Resolver Functions', () => {
   describe('createFlashcard', () => {
-    it('Successful card creation', async () => {
+    it('Test 1 - Successful card creation', async () => {
       const req = {
         payload: { front: 'Question', back: 'Answer', hint: 'Hint', locked: false },
         context: { accountId: '123' },
       };
       const jestCardId = `c-${12345}`;
 
-      const result = await createFlashcard(req);
+      const result = await createFlashcard(req); // result will be in the form of return body
+
+      const createdCardData = {
+        id: 'c-12345',
+        front: 'Question',
+        back: 'Answer',
+        hint: 'Hint',
+        locked: false,
+        owner: '123',
+        name: 'Freddie'
+      };
+
+      expect(storage.set).toHaveBeenCalledWith(jestCardId, createdCardData);
 
       expect(result.success).toBe(true);
-      expect(result.card?.id).toBe(jestCardId);
-      expect(result.card?.front).toBe('Question');
-      expect(result.card?.back).toBe('Answer');
-      expect(result.card?.owner).toBe('123')
+      expect(result.card).toEqual(createdCardData);
+    });
+
+    it('Test 2 - unsuccessful card creation - missing front', async () => {
+      const req = {
+        payload: { front: '', back: 'Answer', hint: 'Hint', locked: false },
+        context: { accountId: '123' },
+      };
+      const jestCardId = `c-${12345}`;
+
+      const result = await createFlashcard(req);
+
+      expect(result.success).toBe(false);
+      expect(result.error).toBe('Invalid input: front and back required');
+    });
+
+    it('Test 3 - unsuccessful card creation - missing back', async () => {
+      const req = {
+        payload: { front: 'Question', back: '', hint: 'Hint', locked: false },
+        context: { accountId: '123' },
+      };
+      const jestCardId = `c-${12345}`;
+
+      const result = await createFlashcard(req);
+
+      expect(result.success).toBe(false);
+      expect(result.error).toBe('Invalid input: front and back required');
+    });
+  });
+
+  describe('updateFlashcard', () => {
+    it('Test 1 - successful card update', async () =>{
+      const jestCardId = `c-${12345}`;
+
+      const oldCard = {
+        id: jestCardId,
+        front: '1+1',
+        back: '3',
+        hint: 'use addition',
+        owner: '123',
+        name: 'Freddie',
+        locked: false,
+      };
+
+      (storage.get as jest.Mock).mockResolvedValueOnce(oldCard); // replicating storage get
+      (storage.set as jest.Mock).mockResolvedValue(undefined); // replicating storage set
+
+      const updatedCard = {
+        ...oldCard,
+        back: '2'
+      }
+
+      const req = {
+        payload: { ...updatedCard },
+        context: { accountId: '123' },
+      }
+
+      const result = await updateFlashcard(req);
+
+      expect(storage.set).toHaveBeenCalledWith(jestCardId, updatedCard);
+      expect(result.success).toBe(true);
+      expect(result.card).toEqual(updatedCard);
+    });
+
+    it('Test 2 - unsuccessful card update: locked card', async () =>{
+      const jestCardId = `c-${12345}`;
+
+      const oldCard = {
+        id: jestCardId,
+        front: '1+1',
+        back: '3',
+        hint: 'use addition',
+        owner: '123',
+        name: 'Freddie',
+        locked: true,
+      };
+
+      (storage.get as jest.Mock).mockResolvedValueOnce(oldCard); // replicating storage get
+      (storage.set as jest.Mock).mockResolvedValue(undefined); // replicating storage set
+
+      const updatedCard = {
+        ...oldCard,
+        back: '2'
+      }
+
+      const req = {
+        payload: { ...updatedCard },
+        context: { accountId: '1234' },
+      }
+
+      const result = await updateFlashcard(req);
+
+      expect(result.success).toBe(false);
+      expect(result.error).toEqual("Only owner can edit");
+    });
+
+    it('Test 3 - successful card update: unlocked card but different user is trying to edit', async () =>{
+      const jestCardId = `c-${12345}`;
+
+      const oldCard = {
+        id: jestCardId,
+        front: '1+1',
+        back: '3',
+        hint: 'use addition',
+        owner: '123',
+        name: 'Freddie',
+        locked: false,
+      };
+
+      (storage.get as jest.Mock).mockResolvedValueOnce(oldCard); // replicating storage get
+      (storage.set as jest.Mock).mockResolvedValue(undefined); // replicating storage set
+
+      const updatedCard = {
+        ...oldCard,
+        back: '2'
+      }
+
+      const req = {
+        payload: { ...updatedCard },
+        context: { accountId: '1234' },
+      }
+
+      const result = await updateFlashcard(req);
+
+      expect(result.success).toBe(true);
+      expect(result.card).toEqual(updatedCard);
     });
   });
 });
